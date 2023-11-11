2c5134c8cf277cc39dd2ffb560a8a4308e8731f6
Yao Zhao <dragonlinux@gmail.com>
2023-11-11 13:28:28 -0500

    update for 11.7, 11.10 add scheduling algorithm to display score, which family has lowest score should be next driver

--- data/data.md
+++ data/data.md
@@ -12,3 +12,7 @@
 |2023.10.19| dylan,sophie,lucas | dylan,sophie,lucas| dylan's mom|
 |2023.10.24| dylan,lucas  | dylan,lucas  | lucas's dad|
 |2023.10.26| dylan,lucas  | dylan,lucas  | dylan's mom|
+|2023.10.31| dylan,lucas  | dylan,lucas  | lucas's mom|
+|2023.11.2 | lucas  | lucas  | lucas's mom|
+|2023.11.7 | dylan,sophie,lucas | dylan,sophie,lucas | sophie's dad | 
+|2023.11.9 | sophie,lucas | sophie,lucas | lucas's dad | 
--- src/main.rs
+++ src/main.rs
@@ -7,56 +7,112 @@ const MAX_COLUMNS: usize = 5;
 const DATA_COLUMN: usize = 1;
 const DATA_ROW: usize = 2;
 
-#[derive(Default, Clone)]
+#[derive(Default, Clone, Copy)]
 struct Stat {
     going: u32,
-    come_back: u32,
-    drives: u32,
+    back: u32,
+    drive: u32,
+    score: i32,
 }
 
 fn read_data_file(file_path: &str) -> Result<String> {
     fs::read_to_string(file_path)
 }
 
-fn parse_cols(cols: &Vec<&str>, collected_data: &mut HashMap<String, Stat>) -> Result<()> {
-    let mut col = DATA_COLUMN;
-    let date = cols[col];
-    col += 1;
-    let going = cols[col].trim_start().trim_end();
-    col += 1;
-    let come_back = cols[col].trim_start().trim_end();
-    col += 1;
-    let driver = cols[col].trim_start().trim_end();
+struct Record<'a> {
+    date: &'a str,
+    going: &'a str,
+    back: &'a str,
+    driver: &'a str,
+}
 
-    for name in going.split(',') {
-        collected_data
-            .entry(name.to_string())
-            .or_insert(Stat::default())
-            .going += 1;
-    }
-
-    for name in come_back.split(',') {
-        collected_data
-            .entry(name.to_string())
-            .or_insert(Stat::default())
-            .come_back += 1;
-    }
-    for name in driver.split(',') {
-        match name.split('\'').next() {
-            Some(child_name) => {
-                collected_data
-                    .entry(child_name.to_string())
-                    .or_insert(Stat::default())
-                    .drives += 1
-            }
-            None => eprintln!("unable to analyze {} driver info.", driver),
-        };
-    }
+struct RecordWidth(usize, usize, usize, usize);
+fn get_record_print_width() -> RecordWidth {
+    let date_width = "2023.10.10".chars().count();
     let go_width = 18;
     let back_width = go_width;
+    let driver_width = go_width;
+
+    RecordWidth(date_width, go_width, back_width, driver_width)
+}
+
+// total_families_least_common_mul=least_common_mul(1...N)
+//  if N=3, 1,2,3's least common mul is 6
+// driver credit: total_families_least_common_mul*(families-1)/families
+// passenger debit: total_families_least_common_mul/families
+// total_families: families in the whole car pool
+// families: one record families
+fn update_score(family_stat: &mut Stat, family_name: &str, driver_name: &str, total_families: u32, families: u32) {
+    if family_name.eq(driver_name) {
+        family_stat.score += (total_families * (families - 1)/ families) as i32;
+    } else {
+        family_stat.score -= (total_families / families) as i32;
+    }
+}
+
+fn parse_cols(cols: &Vec<&str>, collected_data: &mut HashMap<String, Stat>) -> Result<()> {
+    let col = DATA_COLUMN;
+
+    let record = Record {
+        date: cols[col],
+        going: cols[col + 1].trim_start().trim_end(),
+        back: cols[col + 2].trim_start().trim_end(),
+        driver: cols[col + 3].trim_start().trim_end(),
+    };
+
+    let mut driver_family_name: String = String::new();
+    // let mut driver_family = Stat {
+    //     going: 0,
+    //     back: 0,
+    //     drive: 0,
+    //     score: 0,
+    // };
+    for name in record.driver.split(',') {
+        match name.split('\'').next() {
+            Some(child_name) => {
+                let family = collected_data
+                    .entry(child_name.to_string())
+                    .or_insert(Stat::default());
+                family.drive += 1;
+                if driver_family_name.is_empty() {
+                    driver_family_name = child_name.to_string();
+                    // driver_family = *family;
+                } else {
+                    panic!("Only support 1 family driver!");
+                }
+            }
+            None => eprintln!("unable to analyze {} driver info.", record.driver),
+        };
+    }
+
+    let going_count = record.going.split(',').count() as u32;
+    for name in record.going.split(',') {
+        let family = collected_data
+            .entry(name.to_string())
+            .or_insert(Stat::default());
+        family.going += 1;
+        update_score(family, name, &driver_family_name, 6, going_count);
+    }
+
+    let back_count = record.back.split(',').count() as u32;
+    for name in record.back.split(',') {
+        let family = collected_data
+            .entry(name.to_string())
+            .or_insert(Stat::default());
+        family.back += 1;
+        update_score(family, name, &driver_family_name, 6, back_count);
+    }
+
+    // update score back
+    // let family = collected_data
+        // .entry(driver_family_name)
+        // .or_insert(Stat::default());
+    // family.score = driver_family.score;
+
+    let RecordWidth(date_width, go_width, back_width, driver_width) = get_record_print_width();
     println!(
-        "date:{} going:{:go_width$} back:{:back_width$} driver:{}",
-        date, going, come_back, driver,
+        "{:date_width$} {:go_width$} {:back_width$} {:driver_width$}",
+        record.date, record.going, record.back, record.driver,
     );
     Ok(())
 }
@@ -66,6 +122,18 @@ fn parse_rows(rows: &Vec<&str>) -> Option<HashMap<String, Stat>> {
     // markdown format
     // 0. title
     // 1. |----|
+
+    // print title
+    if !rows.is_empty() {
+        let RecordWidth(date_width, go_width, back_width, driver_width) = get_record_print_width();
+
+        println!(
+            "{:date_width$} {:go_width$} {:back_width$} {:driver_width$}",
+            "date", "going", "back", "driver"
+        );
+    }
+
+    // print data
     for row in &rows[DATA_ROW..] {
         let cols: Vec<&str> = row.trim_start().trim_end().split('|').collect();
         if cols.is_empty() || cols.len() < MAX_COLUMNS {
@@ -95,15 +163,16 @@ fn parse_data_file(file_path: &str) -> Option<HashMap<String, Stat>> {
 }
 
 fn analyze(stats: &HashMap<String, Stat>) {
-    println!("family  going back drive");
+    println!();
+    println!("family  going back drive score");
     if stats.is_empty() {
         return;
     }
 
     for (name, s) in stats.iter() {
         println!(
-            "{:<6}  {:<5} {:<4} {}",
-            name, s.going, s.come_back, s.drives
+            "{:<6}  {:<5} {:<4} {:<5} {:<5}",
+            name, s.going, s.back, s.drive, s.score
         );
     }
 }
